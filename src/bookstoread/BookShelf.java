c71d530caf009323b0b2a83bee7625e81c3e789c
Dansou lionel josue <lioneldansou222@gmail.com>
2026-05-06 10:46:51 +0000

    ajout d'un test pour vérifier l'immuabilité de la liste retournée par books()

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,8 +1,17 @@
 package bookstoread;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 public class BookShelf {
+    private final List<String> books = new ArrayList<>();
     public List<String> books() {
-        return Collections.emptyList();
+        return Collections.unmodifiableList(books);
     }
+
+    public void add(String... booksToAdd) {
+        Arrays.stream(booksToAdd).forEach(book ->
+                books.add(book));
+    }
+
 }
--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,10 +1,10 @@
 package bookstoread;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BookShelfSpec {
     @Test
@@ -12,5 +12,31 @@ public class BookShelfSpec {
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+    @Test
+    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java","Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+    }
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+        BookShelf shelf = new BookShelf();
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+    @Test
+    void booksReturnedFromBookShelfIsImmutableForClient() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        try {
+            books.add("The Mythical Man-Month");
+            fail(() -> "Should not be able to add book to books");
+        } catch (Exception e) {
+            assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
+        }
     }
 }
